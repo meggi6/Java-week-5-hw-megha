d02ffae92df0eb1a526e2c548fa7605ecb6973ae
meggi6 <mvij881@gmail.com>
2023-11-17 18:54:21 +0000

    java-week5-wednesday-hw

---
+++ src/java_week5_hw/Programme_2_MarkSheet.java
@@ -0,0 +1,96 @@
+package java_week5_hw;
+/**
+ * Rewrite the student mark sheet programme using if else and while loop. (Week-7 Homework)
+ */
+
+import java.util.Scanner;
+
+public class Programme_2_MarkSheet {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter student Name : "); //input wizard
+        String name = scanner.next();
+        System.out.print("Enter Student Roll Number : "); //input wizard
+        int rollNum = scanner.nextInt();
+        System.out.print("Enter Marks of Subject Maths : "); //input wizard
+        int mathsMarks = scanner.nextInt();
+        while (mathsMarks < 0 || mathsMarks > 100) {
+            System.out.print("Invalid Input, Mark should beween 0 to 100");
+            System.out.print("Please enter correct marks of Maths : ");
+            mathsMarks = scanner.nextInt();
+        }
+        System.out.print("Enter Marks of Subject Science : ");
+        int scienceMarks = scanner.nextInt();
+        while (scienceMarks < 0 || scienceMarks > 100) {
+            System.out.print("Invalid Input, Mark should beteen 0 to 100");
+            System.out.print("PLease enter correct marks of Science :");
+            scienceMarks = scanner.nextInt();
+        }
+        System.out.print("Enter Marks of Subject English : ");
+        int englishMarks = scanner.nextInt();
+        while (englishMarks < 0 || englishMarks > 100) {
+            System.out.print("Invalid Input, Mark should between 0 to 100");
+            System.out.print("Please enter correct marks of English : ");
+            englishMarks = scanner.nextInt();
+        }
+        int total = sum(mathsMarks, scienceMarks, englishMarks);
+        int percentage = (total * 100) / 300;
+        String result = calculateResult(mathsMarks, scienceMarks, englishMarks);
+        String grade = calculateGrade(percentage, result);
+        printTheMarkSheet(name, rollNum, mathsMarks, scienceMarks, englishMarks, total, percentage, result, grade);
+        scanner.close();
+    }
+
+    //calculating sum
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    //Calculating the results
+    public static String calculateResult(int mathMarks, int scienceMarks, int englishMqarks) {
+        if (mathMarks < 35 || scienceMarks < 35 || englishMqarks < 35) {
+            return "FAIL";
+        } else
+            return "PASS";
+    }
+
+    //Calculating the grade on percentage and result
+    public static String calculateGrade(int percentage, String result) {
+        String grade = null;
+        if (result.equalsIgnoreCase("pass")) {
+            grade = "A+";
+        } else if (percentage >= 60) {
+            grade = "A";
+        } else if (percentage >= 50) {
+            grade = "B";
+        } else if (percentage >= 35) {
+            grade = "C";
+        } else {
+            grade = "-";
+        }
+        return grade;
+    }
+
+    // Printing the mark sheet
+    public static void printTheMarkSheet(String name, int rollNum, int mathsMarks, int scienceMarks, int englishMarks, double total, double percentage, String result, String grade) {
+        System.out.println("|----------------------------------------|");
+        System.out.println("|             MARK SHEET                 |");
+        System.out.println("|----------------------------------------|");
+        System.out.println("| Name          : " + name + "           |");
+        System.out.println("| Roll No       : " + rollNum + "        |");
+        System.out.println("|----------------------------------------|");
+        System.out.println("| Subjects      :   Marks                |");
+        System.out.println("|----------------------------------------|");
+        System.out.println("| Math          : " + mathsMarks + "     |");
+        System.out.println("| Science       : " + scienceMarks + "   |");
+        System.out.println("| English       : " + englishMarks + "   |");
+        System.out.println("|----------------------------------------|");
+        System.out.println("| Total         : " + total + "          |");
+        System.out.println("|----------------------------------------|");
+        System.out.println("| Percentage    : " + percentage + "     |");
+        System.out.println("| Result    : " + result + "             |");
+        System.out.println("| Grade    : " + grade + "               |");
+        System.out.println("|----------------------------------------|");
+    }
+}
